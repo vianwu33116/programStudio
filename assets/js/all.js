a9cc3a0073013c63199e39eb0ce36b8369d339d5
vianwu33116 <wserena33116@gmail.com>
2022-08-23 22:25:07 +0800

    Update 2022-08-23T14:24:59.844Z

---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log('Hello');
+$(document).ready(function () {
+  $(".ham-icon").click(function () {
+    $(".header-links").toggle();
+  });
+});
+//# sourceMappingURL=all.js.map
